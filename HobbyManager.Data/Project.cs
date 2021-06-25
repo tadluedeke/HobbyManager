2e38fcda4743dbcf41877cad70b400681585b7bd
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-25 11:43:27 -0400

    Added Project and Workflow objects

---
+++ HobbyManager.Data/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyManager.Data
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public DateTime FinishDate { get; set; }
+    }
+}
---
+++ HobbyManager.Data/Workflow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyManager.Data
+{
+    public class Workflow
+    {
+        [Key]
+        public int WorkflowId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        public Paint Prime { get; set; }
+        public List<Paint> BaseCoat { get; set; }
+        public List<Paint> Shade { get; set; }
+        public List<Paint> HightlightOne { get; set; }
+        public List<Paint> HighlightTwo { get; set; }
+    }
+}
