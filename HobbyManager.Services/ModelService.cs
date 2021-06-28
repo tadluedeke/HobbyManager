1634064b38a655e50cfa5cc99121aafd9ef7c8a2
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-28 11:06:02 -0400

    Worked on ModelCreate in ModelService

---
+++ HobbyManager.Services/ModelService.cs
@@ -0,0 +1,60 @@
+﻿using HobbyManager.Data;
+using HobbyManager.Models;
+using HobbyManager.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobbyManager.Services
+{
+    public class ModelService
+    {
+        private readonly Guid _userId;
+
+        public ModelService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateModel(ModelCreate model)
+        {
+            var entity =
+                new Model()
+                {
+                    OwnerId = _userId,
+                    Name = model.Name,
+                    Scale = model.Scale,
+                    Brand = model.Brand
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Models.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<ModelListItem> GetModels()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Models
+                    .Where(e => e.OwnerId == _userId)
+                    .Select(
+                        e =>
+                        new ModelListItem
+                        {
+                            ModelId = e.ModelId,
+                            Name = e.Name,
+                            Brand = e.Brand
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
