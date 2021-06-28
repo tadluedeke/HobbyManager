ecc51f5f5c79393b641e76273fadc8eac66f64e9
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-28 10:09:08 -0400

    Added ModelListItem and Index View

---
+++ HobbyManager.WebMVC/Controllers/ModelController.cs
@@ -0,0 +1,20 @@
+﻿using HobbyManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HobbyManager.WebMVC.Controllers
+{
+    [Authorize]
+    public class ModelController : Controller
+    {
+        // GET: Model
+        public ActionResult Index()
+        {
+            var model = new ModelListItem[0];
+            return View(model);
+        }
+    }
+}
