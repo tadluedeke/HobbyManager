926a9df4f9d1f81c6e6238b23525d10422ebefc8
Tad Luedeke <tad.luedeke@gmail.com>
2021-07-08 13:02:59 -0400

    Finished editing views, changed .CSS files out for a Bootswatch theme.

--- HobbyManager.WebMVC/App_Start/BundleConfig.cs
+++ HobbyManager.WebMVC/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace HobbyManager.WebMVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-slate.css",
                       "~/Content/site.css"));
         }
     }
